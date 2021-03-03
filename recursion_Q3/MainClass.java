c916c6bccc41b6426d197c6b0821ac038f17ad75
Bharat Gupta <bharat.gupta@metacube.com>
2021-03-03 14:10:02 +0530

    NQueens Updated

--- recursion_Q3/MainClass.java
+++ recursion_Q3/MainClass.java
@@ -1,13 +1,40 @@
 package recursion_Q3;
 
-import java.util.Scanner;
+import static java.lang.System.out;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class MainClass {
+	
+	/**
+	 * To read integer from input stream and check for wrong inputs.
+	 * @return
+	 * 		return input integer.
+	 */
+	public static int readInteger(){
+		int userChoice;
+		InputStreamReader userInput = new InputStreamReader(System.in);
+		BufferedReader bufferedReader = new BufferedReader(userInput);
+		while(true){
+			try{
+				userChoice = Integer.parseInt(bufferedReader.readLine());
+				break;
+			}catch(Exception e){
+				out.println("Wrong Input Type. Please try again.");
+			}
+		}
+		return userChoice;
+	}
+	
+	
 	public static void main(String[] args) {
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("Please Enter Board Size for NQueen Problem: ");
-		int noOfQueens = userInput.nextInt();
-		NQueens.getResult(noOfQueens);
-		userInput.close();
+		out.println("Please Enter Board Size for NQueen Problem: ");
+		int noOfQueens = readInteger();
+		if(noOfQueens<=0){
+			out.println("that is not a valid board size.");
+		}else{
+			NQueens.getResult(noOfQueens);			
+		}
 	}
 }
--- recursion_Q3/NQueens.java
+++ recursion_Q3/NQueens.java
@@ -1,49 +1,75 @@
 package recursion_Q3;
 
+import static java.lang.System.out;
+
 public class NQueens {
 	
-	private static boolean isSafe(int[][] board, int row, int col, int number){
-		for(int x=0; x<row; x++){
-			if(board[x][col]==1){
+	/**
+	 * isSafe function checks whether the position is safe or not for a queen.
+	 * @param board
+	 * 			2D array of 0 & 1 representing chess board.
+	 * @param curRow
+	 * 			current row on which we are checking
+	 * @param curCol
+	 * 			current column on which we are checking
+	 * @param size
+	 * 			size of the chess board or no of queens.
+	 * @return boolean
+	 * 			true if position is safe else false
+	 */
+	private static boolean isSafe(int[][] board, int curRow, int curCol, int size){
+		for(int row=0; row<curRow; row++){
+			if(board[row][curCol]==1){
 				return false;
 			}
 		}
 		
-		int x = row;
-		int y = col;
-		
-		while(x>=0 && y>=0){
-			if(board[x][y]==1){
+		int diagonalRow = curRow;
+		int diagonalCol = curCol;
+		// checking left diagonal
+		while(diagonalRow>=0 && diagonalCol>=0){
+			if(board[diagonalRow][diagonalCol]==1){
 				return false;
 			}
-			x--;
-			y--;
+			diagonalRow--;
+			diagonalCol--;
 		}
 		
-		x = row;
-		y = col;
-		
-		while(x>=0 && y<number){
-			if(board[x][y]==1){
+		diagonalRow = curRow;
+		diagonalCol = curCol;
+		// checking right diagonal
+		while(diagonalRow>=0 && diagonalCol<size){
+			if(board[diagonalRow][diagonalCol]==1){
 				return false;
 			}
-			x--;
-			y++;
+			diagonalRow--;
+			diagonalCol++;
 		}
 		return true;
 	}
 	
 	
-	private static boolean nQueen(int[][] board, int row, int number){
-		if(row>=number){
+	/**
+	 * Recursive NQueen function
+	 * @param board
+	 * 			2D array of 0 & 1 representing chess board.
+	 * @param row
+	 * 			current row in which we are putting our queen
+	 * @param size
+	 * 			size of the chess board.
+	 * @return boolean
+	 * 			true if we place a queen successfully otherwise false.
+	 */
+	private static boolean nQueen(int[][] board, int row, int size){
+		if(row>=size){
 			return true;
 		}
 		
-		for(int col=0; col<number; col++){
-			if(isSafe(board, row, col, number)){
+		for(int col=0; col<size; col++){
+			if(isSafe(board, row, col, size)){
 				board[row][col]=1;
 				
-				if(nQueen(board, row+1, number)){
+				if(nQueen(board, row+1, size)){  // recursion
 					return true;
 				}
 				
@@ -52,19 +78,26 @@ public class NQueens {
 		}
 		return false;
 	}
-	public static void getResult(int number){
-		int[][] board = new int[number][number];
+	
+	
+	/**
+	 * To display the result if possible.
+	 * @param size
+	 * 			the size of the chess board or number of queens.
+	 */
+	public static void getResult(int size){
+		int[][] board = new int[size][size];
 		
-		if(nQueen(board, 0,number)){
-			System.out.println("Solution for given board size: ");
-			for(int row=0; row<number; row++){
-				for(int col=0; col<number; col++){
+		if(nQueen(board, 0,size)){
+			out.println("Solution for given board size: ");
+			for(int row=0; row<size; row++){
+				for(int col=0; col<size; col++){
 					System.out.print(board[row][col] + " ");
 				}
-				System.out.println();
+				out.println();
 			}
 		}else{
-			System.out.println("There is no solution for given size.");
+			out.println("There is no solution for given size.");
 		}
 	}
 }
